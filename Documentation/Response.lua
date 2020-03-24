cd8e99ecc28e3fc710601a827e9b9cfcf2bd8e9e
James-A-W <52931391+James-A-W@users.noreply.github.com>
2020-03-24 22:50:22 +0000

    Create Response.lua

---
+++ Documentation/Response.lua
@@ -0,0 +1,9 @@
+--[[
+A response is what is always returned
+
+Status
+- This is if the response was successful or not
+
+Response
+- This is the actual response, it is either an error object or other data
+]]--
