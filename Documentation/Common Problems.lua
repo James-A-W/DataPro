e4c8bf1626e2044c09ab7a0e50f1da1620b38bac
James-A-W <52931391+James-A-W@users.noreply.github.com>
2020-03-24 22:51:12 +0000

    Create Common Problems.lua

---
+++ Documentation/Common Problems.lua
@@ -0,0 +1,7 @@
+--[[
+502: API SERVICES REJECTED REQUEST WITH ERROR: HTTP 403 (FORBIDDEN)
+- This error is unfixable as far as I know
+- If you get this error, don't worry. The script works in-game just not in studio
+
+IF YOU KNOW ANY MORE PROBLEMS CONTACT ME
+]]--
