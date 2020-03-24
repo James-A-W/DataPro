4fd7ae291bee79c656b4967c419b5c626c6e0fd7
James-A-W <52931391+James-A-W@users.noreply.github.com>
2020-03-24 22:51:02 +0000

    Create Error.lua

---
+++ Documentation/Error.lua
@@ -0,0 +1,11 @@
+--[[
+An error object is mainly used in a response object
+
+Type
+- This is the type of the error
+
+Message
+- This is the error message
+
+These are useful for error handling
+]]--
