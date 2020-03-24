b0954944553c57943550e573615989cc2088ca6d
James-A-W <52931391+James-A-W@users.noreply.github.com>
2020-03-24 22:51:31 +0000

    Create AutoSave.lua

---
+++ Documentation/AutoSave.lua
@@ -0,0 +1,7 @@
+--[[
+Auto Save is the settings function
+
+If AutoSave is enabled inside of Settings then the initiator runs this module
+
+This module saves every players data every X seconds.
+]]--
