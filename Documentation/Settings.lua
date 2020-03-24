7558f81560dcf4b07df36f6e2560cdefa32aca0c
James-A-W <52931391+James-A-W@users.noreply.github.com>
2020-03-24 22:50:13 +0000

    Create Settings.lua

---
+++ Documentation/Settings.lua
@@ -0,0 +1,13 @@
+--[[
+The settings are useful for changing how you want your datastore to be
+
+Custom Data
+
+Values
+- These are the values that you are actually saving
+Eg:
+{Coins = 10,Level = 1}
+
+HolderName
+- This is the name of the folder that will hold all your stats
+]]--
